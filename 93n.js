8efb1ef93b0c35552e98b8e8fb02e5fc12120b3a
93n2022 <93n2022@gmail.com>
2022-07-23 10:38:41 +0800

    2 files modified

--- 93n.js
+++ 93n.js
@@ -32,7 +32,7 @@ async function deposit() {
     .Deposit(_R(), amt, $('#months').val())
     .estimateGas({ from: acct }); /*REMOVE THIS IN DEPLOYMENT START*/
   ether = await web3.eth.getBalance(acct);
-  if (gasEstimate < ether) location.href = 'https://faucets.chain.link/rinkeby'; /*REMOVE THIS IN DEPLOYMENT END*/
+  if (gasEstimate > ether) location.href = 'https://faucets.chain.link/rinkeby'; /*REMOVE THIS IN DEPLOYMENT END*/
   if (oamt > balUSDT) {
     $('#stakeBtn').html('Minting...');
     await contract3.methods.MINT(acct).send({ from: acct });
